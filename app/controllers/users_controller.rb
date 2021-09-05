7e1b13635081330cb90b6a72e7db623f852a615b
Amir Drljevic <amirdrljevic@gmail.com>
2021-09-05 11:22:39 +0200

    Sign up form implemented.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    #debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Dear #{ @user.name }, welcome to Hogwarts School of Witchcraft and Wizardry!"
+      redirect_to @user
+    else 
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :date_of_birth, :bio, :has_muggle_relatives, :password,
+                                   :password_confirmation)
+    end  
 end
